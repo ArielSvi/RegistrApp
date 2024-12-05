31a6f06a7ee4668e8ac57b6ae5ace0dc3e649c82
Ariel <arie.salinas@duocuc.cl>
2024-12-05 00:43:34 -0300

    correccion de palabras

--- src/app/crearqr/crearqr.page.spec.ts
+++ src/app/crearqr/crearqr.page.spec.ts
@@ -25,28 +25,28 @@ describe('CrearqrPage', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('debería crear', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have the initial value of qrData as an empty string', () => {
+  it('debe tener el valor inicial de qrData como una cadena vacía', () => {
     expect(component.qrData).toBe('');
   });
 
-  it('should assign qrData to createdCode when generateQRCode is called', () => {
+  it('debe asignar qrData a createCode cuando se llama a generateQRCode', () => {
     component.qrData = 'Nuevo código QR';
     component.generateQRCode();
     expect(component.createdCode).toBe('Nuevo código QR');
   });
 
-  it('should not display QR code initially', () => {
+  it('no debería mostrar el código QR inicialmente', () => {
     component.createdCode = '';  // Asegura que no hay QR antes de la generación
     fixture.detectChanges();
     const qrCodeElement = fixture.nativeElement.querySelector('qrcode');
     expect(qrCodeElement).toBeNull();  // No debe haber un código QR antes de generar uno
   });
 
-  it('should update createdCode when generateQRCode is called', () => {
+  it('debería actualizar el código creado cuando se llama a generar QRCode', () => {
     component.qrData = 'Nuevo valor para QR';
     component.generateQRCode();
     expect(component.createdCode).toBe('Nuevo valor para QR');
